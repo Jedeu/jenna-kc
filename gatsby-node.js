874abd17b387cf367a2aeb896f85721b06d793f9
Jed Piezas <jed.piezas@gmail.com>
2021-01-24 16:47:35 -0800

    Remove unused export

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,18 +16,3 @@ exports.createPages = ({ actions }) => {
     isPermanent: true,
   })
 }
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /react-particle-animation/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
